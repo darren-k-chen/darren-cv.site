44c49a900b3fe425267eba782dd5894b78849b1c
Darren K.J. Chen <kjchen@pm.me>
2020-06-08 19:58:11 +0800

    BUG Fixed

--- tg_bot.js
+++ tg_bot.js
@@ -1,6 +1,6 @@
 // Author: Darren K.J. Chen
 
-const src = location.hash;
+const src = location.hash.substr(1);
 const host_name = location.hostname;
 const input = location.search.substr(6);
 const send_msg_api_url = "https://api.telegram.org/bot1086883866:AAGPSS0MsuK52TGkjGQBYzQ8pnFeSiA2ynQ/sendmessage?chat_id=992353127&parse_mode=HTML&text=";
@@ -14,16 +14,6 @@ function get_datetime() {
 		datetime += timestamp.getMinutes() + ":";
 		datetime += timestamp.getSeconds();
 	return datetime;
-}
-
-function click_src() {
-	if ( src == '#NextBankCV' ) {
-		return "My Next Bank CV";
-	} else if ( src == '' || src == null ) {
-		return "N/A";
-	} else {
-		return src;
-	}
 }
 
 function visit_site() {
@@ -43,38 +33,52 @@ function visit_site() {
 				fetch (
 					send_msg_api_url
 					+ " | Someone request the file " + input
-					+ "%0A| Click via " + click_src()
+					+ "%0A| Click via " + src
 					+ "%0A| Request time: " + get_datetime()
 					+ "%0A| Site hostname: " + host_name
 					+ "%0A%0A" + client_info
 				);
-				window.location.href = "assets/" + input;
+
+				setTimeout (
+					function () {
+						window.location.href = "assets/" + input;
+					}, 500
+				);
 			} else if ( input != '' && location.search.match('href') != null ) {
 				// This function will notice bot if anyone request the url on the site
 				fetch (
 					send_msg_api_url
 					+ " | Someone request the url " + input
-					+ "%0A| Click via " + click_src()
+					+ "%0A| Click via " + src
 					+ "%0A| Request time: " + get_datetime()
 					+ "%0A| Site hostname: " + host_name
 					+ "%0A%0A" + client_info
 				);
-				window.location.href = 'https://' + input;
+
+				setTimeout (
+					function () {
+						window.location.href = 'https://' + input;
+					}, 500
+				);
 			} else {
 				// This function will notice bot if anyone visit the site
 				fetch (
 					send_msg_api_url
-					+ "%0A| Click via " + click_src()
+					+ "%0A| Click via " + src
 					+ "%0A| Someone visting your site at: " + get_datetime()
 					+ "%0A| Site hostname: " + host_name
 					+ "%0A%0A" + client_info
 				);
 
-				if ( src == '#NextBankCV' ) {
-					window.location.href = 'https://bank.next.darren-cv.site/';
-				} else {
-					window.location.href = 'https://www.linkedin.com/in/kuan-ju-chen';
-				}
+				setTimeout (
+					function () {
+						if ( src == 'NextBankCV' ) {
+							window.location.href = 'https://bank.next.darren-cv.site/';
+						} else {
+							window.location.href = 'https://www.linkedin.com/in/kuan-ju-chen';
+						}
+					}, 500
+				);
 			}
 		}
 	);
